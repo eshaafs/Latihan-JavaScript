f26badc9d0ec62f25fe67398ab0b30971c0b3b4e
eshaafs <eshaapriafitrasajaka@gmail.com>
2019-11-12 10:41:57 +0700

    mengedit file suitJawa.js

--- suitJawa.js
+++ suitJawa.js
@@ -1,17 +1,43 @@
 // menangkap pilihan player
-var p = prompt('Silakan pilih pilihannya : gajah, semut, orang')
+var main = true;
+while (main) {
+var p = prompt('Silakan pilih pilihannya : gajah, semut, orang');
 
 // menangkap pilihan komputer
 // membangkitkan bilangan random 
 
 var comp = Math.random();
 
-if(comp < 0.34) {
+if ( comp < 0.34 ) {
     comp = 'gajah';
-} else if(comp >= 0.34 && < 0.67) {
+} else if ( comp >= 0.34 && comp < 0.67 ) {
     comp = 'semut';
-} else{
+} else {
     comp = 'orang';
 }
 
-console.log(comp);
+// menentukan rules
+var hasil;
+if ( p == comp ){
+    hasil = 'SERI';
+} else if ( p == 'gajah') {
+    // if ( comp == 'orang' ) {
+    //     hasil = 'MENANG';
+    // } else {
+    //     hasil = 'KALAH';
+    // }
+    hasil = ( comp == 'orang' ) ? 'MENANG' : 'KALAH';
+} else if ( p == 'semut' ) {
+    hasil = ( comp == 'gajah') ? 'MENANG' : 'KALAH';
+} else if ( p == 'orang' ) {
+    hasil = ( comp == 'semut' ) ? 'MENANG' : 'KALAH';
+} else {
+    hasil = 'Anda tidak memasukkan pilihan dengan benar!';
+}
+
+// Menampilkan hasil
+alert("Kamu memilih '" + p + "' sedangkan komputer memilih '" + comp + "', maka hasilnya kamu " + hasil);
+main = confirm('Ingin main lagi ?');
+}
+
+alert('Terimakasih sudah bermain');
